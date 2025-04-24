3954d2e2980b3cb9284dbda4d3e45b473f6d3907
MatthewSo <matthew.p.v.so@gmail.com>
2025-04-24 11:47:32 -0400

    Updating single task

--- cifar10_entry_point_single_task.py
+++ cifar10_entry_point_single_task.py
@@ -21,7 +21,7 @@ PRIMARY_LEARNING_RATE = 0.01
 PPO_LEARNING_RATE = 0.0003
 SCHEDULER_STEP_SIZE = 50
 SCHEDULER_GAMMA = 0.5
-AUX_WEIGHT = 1
+AUX_WEIGHT = 0
 TRAIN_RATIO = 1
 
 git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
