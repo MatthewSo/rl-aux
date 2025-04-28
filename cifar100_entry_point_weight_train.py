985aa64968313d0e36f1e492df0be24868eca673
MatthewSo <matthew.p.v.so@gmail.com>
2025-04-28 13:38:22 -0400

    Fixing import

--- cifar100_entry_point_weight_train.py
+++ cifar100_entry_point_weight_train.py
@@ -7,6 +7,7 @@ from torch import nn
 
 from datasets.cifar100 import CIFAR100, CoarseLabelCIFAR100
 from datasets.transforms import trans_train, trans_test
+from environment.learn_weight_aux_task import AuxTaskEnv
 from networks.ppo.ppo import get_ppo_agent
 from networks.primary.vgg import VGG16
 from train.train_auxilary_agent import train_auxilary_agent
