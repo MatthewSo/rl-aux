a48a99f5d83dcb1610e0af53cd0bff4c92689a91
MatthewSo <matthew.p.v.so@gmail.com>
2025-05-05 19:05:45 -0400

    Adding more experiments and tests

---
+++ cifar10_entry_point_learn_no_aux.py
@@ -0,0 +1,149 @@
+import subprocess
+
+import torch
+from torch import nn
+
+from datasets.cifar10 import CIFAR10
+from datasets.transforms import  cifar_trans_train, cifar_trans_test
+from environment.learn_weight_aux_task import AuxTaskEnv
+from networks.ppo.ppo import get_ppo_agent
+from networks.primary.vgg import VGG16
+from train.train_auxilary_agent import train_auxilary_agent
+from utils.log import log_print, change_log_location
+from utils.path_name import create_path_name, save_all_parameters
+
+BATCH_SIZE = 100
+AUX_DIMENSION = 50
+PRIMARY_DIMENSION = 10
+OBSERVATION_FEATURE_DIMENSION = 256
+TOTAL_EPOCH = 200
+PRIMARY_LEARNING_RATE = 0.01
+PPO_LEARNING_RATE = 0.0003
+SCHEDULER_STEP_SIZE = 50
+SCHEDULER_GAMMA = 0.5
+AUX_WEIGHT = 0
+TRAIN_RATIO = 1
+LEARN_WEIGHTS = False
+
+git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+
+SAVE_PATH = create_path_name(
+    agent_type="SINGLETASK",
+    primary_model_type="VGG",
+    train_ratio=TRAIN_RATIO,
+    aux_weight=AUX_WEIGHT,
+    learn_weights=LEARN_WEIGHTS,
+    observation_feature_dimensions=OBSERVATION_FEATURE_DIMENSION,
+    dataset="CIFAR10",
+)
+
+change_log_location(SAVE_PATH)
+
+save_all_parameters(
+    batch_size=BATCH_SIZE,
+    aux_dimensions=AUX_DIMENSION,
+    primary_dimensions=PRIMARY_DIMENSION,
+    total_epoch=TOTAL_EPOCH,
+    primary_learning_rate=PRIMARY_LEARNING_RATE,
+    rl_learning_rate=PPO_LEARNING_RATE,
+    scheduler_step_size=SCHEDULER_STEP_SIZE,
+    scheduler_gamma=SCHEDULER_GAMMA,
+    aux_weight=AUX_WEIGHT,
+    learn_weights=LEARN_WEIGHTS,
+    train_ratio=TRAIN_RATIO,
+    save_path=SAVE_PATH,
+    dataset="CIFAR10",
+    model_name="VGG",
+    agent_type="PPO",
+    observation_feature_dimensions=OBSERVATION_FEATURE_DIMENSION,
+    aux_task_type="AuxTask",
+    primary_task_type="VGG",
+    git_commit_hash=git_hash,
+)
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+log_print("Torch CUDA available:", torch.cuda.is_available())
+log_print("Torch CUDA device count:", torch.cuda.device_count())
+device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+
+log_print("Using device:", device)
+
+# ---------
+
+train_set = CIFAR10(
+    root="./data/cifar10",
+    train=True,
+    transform=cifar_trans_train
+)
+test_set = CIFAR10(
+    root="./data/cifar10",
+    train=False,
+    transform=cifar_trans_test
+)
+
+train_dataloader = torch.utils.data.DataLoader(
+    dataset=train_set,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+)
+
+test_dataloader = torch.utils.data.DataLoader(
+    dataset=test_set,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+)
+
+
+primary_model = VGG16(
+    primary_task_output=PRIMARY_DIMENSION,
+    auxiliary_task_output=AUX_DIMENSION
+).to(device)
+criterion = nn.CrossEntropyLoss()
+
+optimizer_callback = lambda x: torch.optim.SGD(x.parameters(), lr=PRIMARY_LEARNING_RATE)
+scheduler_callback = lambda x: torch.optim.lr_scheduler.StepLR(x, step_size=SCHEDULER_STEP_SIZE, gamma=SCHEDULER_GAMMA)
+
+env = AuxTaskEnv(
+    train_dataset=train_set,
+    device=device,
+    model=primary_model,
+    criterion=criterion,
+    optimizer_func=optimizer_callback,
+    scheduler_func=scheduler_callback,
+    batch_size=BATCH_SIZE,
+    pri_dim=PRIMARY_DIMENSION,
+    aux_dim=AUX_DIMENSION,
+    aux_weight=AUX_WEIGHT,
+    save_path=SAVE_PATH,
+    learn_weights=LEARN_WEIGHTS,
+    verbose=True,
+)
+
+auxilary_task_agent = get_ppo_agent(env=env,
+                                    feature_dim=OBSERVATION_FEATURE_DIMENSION,
+                                    auxiliary_dim=AUX_DIMENSION,
+                                    learning_rate=PPO_LEARNING_RATE,
+                                    device=device,
+                                    ent_coef=0.01,
+                                    n_steps=BATCH_SIZE,
+                                    n_epochs=10,
+                                    batch_size=BATCH_SIZE,
+                                    weight_bins=21,
+                                    )
+
+log_print("Done Initializing PPO Agent")
+
+# Train the PPO agent
+train_auxilary_agent(
+    primary_model=primary_model,
+    rl_model=auxilary_task_agent,
+    env=env,
+    device=device,
+    test_loader=test_dataloader,
+    batch_size=BATCH_SIZE,
+    total_epochs=TOTAL_EPOCH,
+    save_path=SAVE_PATH,
+    model_train_ratio=TRAIN_RATIO,
+    primary_dimension=PRIMARY_DIMENSION,
+    skip_rl=True
+)
