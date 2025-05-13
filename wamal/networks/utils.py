ff1b31bcc091369089bdbe9620c8deb9aaac8ba7
MatthewSo <matthew.p.v.so.99@gmail.com>
2025-05-13 16:41:16 -0400

    Fixing self reference

--- wamal/networks/utils.py
+++ wamal/networks/utils.py
@@ -13,7 +13,7 @@ from train.model.performance import EpochPerformance
 from utils.log import change_log_location, log_print
 from utils.path_name import create_path_name
 
-def model_fit(self, x_pred, x_output, pri=True, num_output=3):
+def model_fit(x_pred, x_output, pri=True, num_output=3):
     if not pri:
         # generated auxiliary label is a soft-assignment vector (no need to change into one-hot vector)
         x_output_onehot = x_output
@@ -26,7 +26,7 @@ def model_fit(self, x_pred, x_output, pri=True, num_output=3):
     loss = x_output_onehot * (1 - x_pred)**2 * torch.log(x_pred + 1e-20)
     return torch.sum(-loss, dim=1)
 
-def model_entropy(self, x_pred1):
+def model_entropy(x_pred1):
     # compute entropy loss
     x_pred1 = torch.mean(x_pred1, dim=0)
     loss1 = x_pred1 * torch.log(x_pred1 + 1e-20)
