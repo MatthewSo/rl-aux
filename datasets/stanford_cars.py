9cc99fb2d66445dedb92bdb3a4df6ebaee9e1c53
MatthewSo <matthew.p.v.so@gmail.com>
2025-05-13 23:09:44 -0400

    Adding stanford

--- datasets/stanford_cars.py
+++ datasets/stanford_cars.py
@@ -6,21 +6,17 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.datasets import OxfordIIITPet as OxfordIIITPetOriginal
 from torchvision.datasets import StanfordCars as StanfordCarsOriginal
-
+from datasets import load_dataset
 
 class StanfordCars(Dataset):
     def __init__(self, root, train=True, transform=None, download=False):
         split = "train" if train else "test"
-        self.cars = StanfordCarsOriginal(
-            root=root,
-            split=split,
-            transform=transform,
-            download=download,
-        )
+        dataset = load_dataset("tanganke/stanford_cars")
+        self.dataset = dataset[split]
 
     def __len__(self):
-        return len(self.cars)
+        return len(self.dataset)
 
     def __getitem__(self, idx):
-        img, class_id = self.cars[idx]
-        return img, class_id
+        img, class_id = self.dataset[idx]
+
