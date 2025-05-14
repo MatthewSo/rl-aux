33d13a54d54401dbc78d6c82aa670edeadfd3187
MatthewSo <matthew.p.v.so@gmail.com>
2025-05-13 23:13:24 -0400

    Putting pets back

--- datasets/stanford_cars.py
+++ datasets/stanford_cars.py
@@ -4,20 +4,23 @@ import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
-from torchvision.datasets import OxfordIIITPet as OxfordIIITPetOriginal
 from torchvision.datasets import StanfordCars as StanfordCarsOriginal
-from torchvision import datasets
-from datasets import load_dataset
+
 
 class StanfordCars(Dataset):
+    """Stanford Cars dataset wrapper."""
     def __init__(self, root, train=True, transform=None, download=False):
         split = "train" if train else "test"
-        dataset = load_dataset("tanganke/stanford_cars")
-        self.dataset = dataset[split]
+        self.cars = StanfordCarsOriginal(
+            root=root,
+            split=split,
+            transform=transform,
+            download=download,
+        )
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.cars)
 
     def __getitem__(self, idx):
-        img, class_id = self.dataset[idx]
-
+        img, class_id = self.cars[idx]     # already (image, int) in torchvision
+        return img, class_id
