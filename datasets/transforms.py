bac207a67a51d89d58d102c57b97d65359507053
MatthewSo <matthew.p.v.so@gmail.com>
2025-05-14 15:35:30 -0400

    Trying to use different shape

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -69,15 +69,15 @@ imgnet_mean = (0.485, 0.456, 0.406)
 imgnet_std  = (0.229, 0.224, 0.225)
 
 common_train_tf = transforms.Compose([
-    transforms.RandomResizedCrop(112, scale=(0.8, 1.0)),
+    transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(imgnet_mean, imgnet_std),
 ])
 
 common_test_tf = transforms.Compose([
-    transforms.Resize(128),
-    transforms.CenterCrop(112),
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(imgnet_mean, imgnet_std),
 ])
