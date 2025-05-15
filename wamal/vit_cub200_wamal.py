1604b7a72659a0fff3a67208ffd7f1954ff529e2
MatthewSo <matthew.p.v.so@gmail.com>
2025-05-15 11:17:34 -0400

    Updating cub200

--- wamal/vit_cub200_wamal.py
+++ wamal/vit_cub200_wamal.py
@@ -32,7 +32,7 @@ GEN_OPTIMIZER_LR = 1e-3
 GEN_OPTIMIZER_WEIGHT_DECAY = 5e-4
 TRAIN_RATIO = 1
 OPTIMIZER = "SGD"
-FULL_DATASET = TrueOPTIMIZER="SGD"
+FULL_DATASET = True
 
 save_path = create_path_name(
     agent_type="WAMAL",
