69d5f0b5d313cf7641e871f776637b3791760547
MatthewSo <matthew.p.v.so@gmail.com>
2025-05-21 14:07:50 -0400

    Updtaing adam

--- wamal/vgg16_cifar_100_20_wamal.py
+++ wamal/vgg16_cifar_100_20_wamal.py
@@ -32,7 +32,7 @@ FULL_DATASET = True
 RANGE = 5
 USE_AUXILIARY_SET = False
 AUXILIARY_SET_RATIO = 0.1
-NORMALIZE_BATCH = True
+NORMALIZE_BATCH = False
 BATCH_FRACTION = None
 
 save_path = create_path_name(
@@ -51,7 +51,7 @@ save_path = create_path_name(
     normalize_batch=NORMALIZE_BATCH,
     batch_fraction=BATCH_FRACTION,
 )
-device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 cifar100_train = CIFAR100(
     root="./data/cifar100",
