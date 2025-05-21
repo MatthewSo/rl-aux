014734e417518f0f4956e9c7aef3dbc4f41c7654
MatthewSo <matthew.p.v.so@gmail.com>
2025-05-20 23:28:09 -0400

    Adding normalized batch

--- wamal/vgg16_cifar_100_20_wamal.py
+++ wamal/vgg16_cifar_100_20_wamal.py
@@ -30,9 +30,9 @@ TRAIN_RATIO = 1
 OPTIMIZER = "SGD"
 FULL_DATASET = True
 RANGE = 5
-USE_AUXILIARY_SET = True
+USE_AUXILIARY_SET = False
 AUXILIARY_SET_RATIO = 0.1
-NORMALIZE_BATCH = False
+NORMALIZE_BATCH = True
 BATCH_FRACTION = None
 
 save_path = create_path_name(
@@ -51,7 +51,7 @@ save_path = create_path_name(
     normalize_batch=NORMALIZE_BATCH,
     batch_fraction=BATCH_FRACTION,
 )
-device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:4" if torch.cuda.is_available() else "cpu")
 
 cifar100_train = CIFAR100(
     root="./data/cifar100",
