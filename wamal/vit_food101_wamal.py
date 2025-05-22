e8285d146e39bd6644d7d2f1a482bd84585fb63d
MatthewSo <matthew.p.v.so@gmail.com>
2025-05-21 23:00:53 -0400

    change gpu

--- wamal/vit_food101_wamal.py
+++ wamal/vit_food101_wamal.py
@@ -55,7 +55,7 @@ save_path = create_path_name(
     normalize_batch=NORMALIZE_BATCH,
     batch_fraction=BATCH_FRACTION,
 )
-device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:4" if torch.cuda.is_available() else "cpu")
 
 train_set = Food101(
     root="./data/food101",
