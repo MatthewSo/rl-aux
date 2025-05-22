e233fc4f1017c26068b8e99fa9b658e10c7afd3a
MatthewSo <matthew.p.v.so@gmail.com>
2025-05-22 00:39:50 -0400

    Updating wamal

--- wamal/vgg16_svhn_wamal.py
+++ wamal/vgg16_svhn_wamal.py
@@ -30,7 +30,7 @@ TRAIN_RATIO = 1
 OPTIMIZER = "SGD"
 RANGE = 5
 FULL_DATASET = True
-USE_AUXILIARY_SET = False
+USE_AUXILIARY_SET = True
 AUXILIARY_SET_RATIO = 0.1
 NORMALIZE_BATCH = False
 BATCH_FRACTION = None
@@ -51,7 +51,7 @@ save_path = create_path_name(
     normalize_batch=NORMALIZE_BATCH,
     batch_fraction=BATCH_FRACTION,
 )
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 
 train_set = SVHN(
     root="./data/svhn",
